5806099d0eaa417dc934442a5774093a307b577a
srimani282 <srimanigoud@gmail.com>
2022-02-15 21:52:00 +0530

    Conditinal Rendering

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   let name = "Sri Mani B";
-  var num = 92928347587467389;
+  let num = 92928347587467389;
+
   return (
     <div className="App">
-      <h1>Heading!!!!!!!!!</h1>
-      <p>name</p>
-      <p>{name}</p>
-      <p>number</p>
-      <p>{num}</p>
+      <h1>Heading</h1>
+      <h1>{name}</h1>
+      {true ? <h3>{num}</h3> : <h3>No number</h3>}
     </div>
   );
 }
